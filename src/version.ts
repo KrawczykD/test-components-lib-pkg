060adddf0e76e45b92ac9275a3f1615dc389e8b5
KrawczykD <40856421+KrawczykD@users.noreply.github.com>
2022-03-29 16:15:04 +0100

    Version bump 0.0.4

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,9 @@
 const appInfo = {
   name: '',
-  releasedOn: 'Tue, 29 Mar 2022 15:07:35 GMT',
-  branch: '938e6c8.types',
+  releasedOn: 'Tue, 29 Mar 2022 15:15:04 GMT',
+  branch: '1fe07d4.types',
   env: '',
-  version: '0.0.3'
+  version: '0.0.4'
 };
 
 export default appInfo;
